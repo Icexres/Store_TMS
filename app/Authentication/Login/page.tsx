37870b9ec0262f2666500fcfafde70e34f0e845d
Icexres <kipuaayush@gmail.com>
2025-09-15 14:01:46 +0545

    added login and signup dummy

---
+++ app/Authentication/Login/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Login = () => {
+  return (<>
+    <div className='Login-container'>
+      <div className='header bg-[#Eff2c0] text-black p-1'>
+        <div className='logo text-4xl font-extrabold'>TMS</div>
+      </div>
+        {/* end of header */}
+    <div className='form'>
+        <form className="items-center bg-[#A4BAB7] shadow-lg rounded-lg p-8 w-full max-w-md mx-auto mt-28">
+            <div className="self-auto text-3xl font-extrabold mb-4 text-gray-800">Log in</div>
+            <label className="block text-gray-700 font-semibold mb-2" htmlFor="email">Email:</label>
+            <input
+            type="text"
+            placeholder="Enter your email"
+            className=" text-black p-3 mb-4 w-full border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#0070f3] transition"
+            />
+            <label className="block text-gray-700 font-semibold mb-2" htmlFor="password">Password:</label>
+            <input
+            type="password"
+            placeholder="Password"
+            className=" text-black p-3 mb-4 w-full border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#0070f3] transition"
+            />
+            <div className="flex justify-center">
+                <button type="submit" className="bg-[#0070f3] hover:bg-[#005bb5] text-white font-semibold py-3 px-6 rounded transition">Login</button>
+            </div>
+        </form>
+        </div>
+    </div>
+    <div className='footer bg-[#Eff2c0] text-black p-4 mt-60 text-center'>
+            <small>&copy; 2025 TMS. All rights reserved.</small>
+        </div>
+  </>
+  )
+}
+
+export default Login
--- app/Landing/page.tsx
+++ app/Landing/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Landing = () => {
   return ( <>
@@ -10,32 +11,18 @@ const Landing = () => {
         <div className='content text-center mt-40 flex flex-col gap-8'>
             <div className='text-4xl font-serif font-extrabold'>Get Started With TMS</div>
             <div className='button-container flex flex-row justify-center gap-20 font-semibold'>
-                <div className='bg-[#Eff2c0] border-2 border-black text-2xl px-4 py-2 rounded-full text-black hover:bg-black hover:border-2 hover:border-[#Eff2c0] hover:text-[#eff2c0] transition-colors duration-300 cursor-pointer'>Login</div>
-                <div className='bg-[#Eff2c0] border-2 border-black text-2xl px-4 py-2 rounded-full text-black hover:bg-black hover:border-2 hover:border-[#Eff2c0] hover:text-[#eff2c0] transition-colors duration-300 cursor-pointer'>Signup</div>
+                <Link href="/Authentication/Login">
+                    <div className='bg-[#Eff2c0] border-2 border-black text-2xl px-4 py-2 rounded-full text-black hover:bg-black hover:border-2 hover:border-[#Eff2c0] hover:text-[#eff2c0] transition-colors duration-300 cursor-pointer'>Login</div>
+                </Link>
+                <Link href="/Authentication/Signup">
+                    <div className='bg-[#Eff2c0] border-2 border-black text-2xl px-4 py-2 rounded-full text-black hover:bg-black hover:border-2 hover:border-[#Eff2c0] hover:text-[#eff2c0] transition-colors duration-300 cursor-pointer'>Signup</div>
+                </Link>
             </div>
         </div>
     {/* end of content */}
-
-
-    {/* <div className='form'>
-        <form className=" items-center bg-[#A4BAB7] shadow-lg rounded-lg p-8 w-full max-w-md mx-auto mt-28">
-            <label className=" self-auto text-3xl font-extrabold mb-6 text-gray-800">Sign Up for TMS</label>
-            <input
-            type="text"
-            placeholder="Enter your email"
-            className="p-3 mb-4 w-full border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#0070f3] transition"
-            />
-            <button
-            type="submit"
-            className="bg-[#0070f3] hover:bg-[#005bb5] text-white font-semibold py-3 px-6 rounded w-full transition"
-            >
-            Get Started
-            </button>
-        </form>
-        </div> */}
     </div>
         <div className='footer bg-[#Eff2c0] text-black p-4 mt-60 text-center'>
-            <small>&copy; 2024 TMS. All rights reserved.</small>
+            <small>&copy; 2025 TMS. All rights reserved.</small>
         </div>
  
 </>  )
